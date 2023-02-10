f9b572293074dfc5d4796e87ee339856fdf922c1
Yusuke Akiyama <akiyamaysk0321@gmail.com>
2023-02-10 14:32:59 +0900

    Add Callapsel and Dialog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/serverless';
+import image from '@astrojs/image';
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-})
+  integrations: [image()]
+});
