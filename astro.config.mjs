38f380274d6b2d46c73ae8074e1c5853fb6b71d1
Yusuke Akiyama <akiyamaysk0321@gmail.com>
2023-02-10 13:03:21 +0900

    Add prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel()
-});
+  output: 'server',
+  adapter: vercel(),
+})
